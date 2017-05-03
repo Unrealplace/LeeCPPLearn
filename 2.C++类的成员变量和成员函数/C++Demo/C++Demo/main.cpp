eeaadc1f996cdabcfc3c51e2d73399ac476c69fb
oliver <oliver@adurolight.com>
2017-05-03 11:57:41 +0800

    类的成员变量和成员函数

---
+++ 2.C++类的成员变量和成员函数/C++Demo/C++Demo/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  C++Demo
+//
+//  Created by MacBook on 2017/5/3.
+//  Copyright © 2017年 Oliver. All rights reserved.
+//
+
+#include <iostream>
+#include "Student.cpp"
+
+int main(int argc, const char * argv[]) {
+
+    Student stu;
+    
+    stu.sleep();
+    stu.say();
+    
+
+    return 0;
+}
