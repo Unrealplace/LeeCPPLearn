77a280b00c24c1c658f08d07eb1171e0694e7b07
LiYang <13837196824@163.com>
2017-05-09 20:59:25 +0800

    第31暂未学习

---
+++ 30.C++向上转型/C++Demo/C++Demo/ObjectClass2.cpp
@@ -0,0 +1,9 @@
+//
+//  ObjectClass2.cpp
+//  C++Demo
+//
+//  Created by LiYang on 17/5/9.
+//  Copyright © 2017年 Oliver. All rights reserved.
+//
+
+#include "ObjectClass2.hpp"
---
+++ 30.C++向上转型/C++Demo/C++Demo/ObjectClass2.hpp
@@ -0,0 +1,18 @@
+//
+//  ObjectClass2.hpp
+//  C++Demo
+//
+//  Created by LiYang on 17/5/9.
+//  Copyright © 2017年 Oliver. All rights reserved.
+//
+
+#ifndef ObjectClass2_hpp
+#define ObjectClass2_hpp
+
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+
+
+#endif /* ObjectClass2_hpp */
